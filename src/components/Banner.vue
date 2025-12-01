00392bf248e1ac870480f3716632db97b53a36c0
Pinheiro-Lorena <lorenapro554@gmail.com>
2025-12-01 10:37:45 -0300

    first commit

---
+++ src/components/Banner.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+
+<section class="banner">
+<h1>
+    Onde a paisagem vira 
+    <span>Inspiração</span>
+</h1>
+<p>Explore paisagens, cores e luzes que inspiram novas histórias através da fotografia.</p>
+</section>
+
+</template>
+
+<style scoped lang ="scss">
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+import logo from '../assets/logo.png'
+</script>
+
+<template>
+<header>
+    <img :src="logo" alt="Logo">
+
+    <nav>
+        <ul>
+        <li>
+            <a href="">
+                <Icon class="icon-search"   icon="material-symbols:search"/>
+            </a>
+        </li><a href="">Inicio</a>
+        <li>
+           
+        </li><a href="">Galeria</a>
+        <li>
+            
+        </li><a href="">Favoritos</a>
+        
+        <li>
+            <a href="">
+                <Icon class="icon"   icon="material-symbols:person"/>
+            </a>
+        </li>
+        </ul>
+    </nav>
+</header>
+</template>
+
+<style scoped lang ="scss">
+header{
+    //border: solid;
+    display: flex;
+   justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    margin: 2rem;
+
+    ul{
+        display: flex;
+        gap: 1rem;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        list-style: none;
+        
+    }
+    a{
+        text-decoration: none;
+        color: black;
+        font-size: 1.2rem;
+        align-items: center;
+    }
+}
+</style>
