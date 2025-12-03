5d206bc206f8514e30834ccab4ffbc6a23776cab
Pinheiro-Lorena <lorenapro554@gmail.com>
2025-12-03 09:43:13 -0300

    atualizações

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,19 +1,46 @@
 <script setup>
-
+import fotografo from "../assets/fotografo (1).png";
 </script>
 
 <template>
 
 <section class="banner">
+<div>
 <h1>
     Onde a paisagem vira 
-    <span>Inspiração</span>
+    <span>Inspiração.</span>
 </h1>
 <p>Explore paisagens, cores e luzes que inspiram novas histórias através da fotografia.</p>
+</div>
+<img :src="fotografo" alt="Fotógrafo com câmera">
 </section>
 
 </template>
 
 <style scoped lang ="scss">
+.banner{
+    //border: solid;
+    display: flex;
+    justify-content: space-between;
+    margin: 2rem;
+
+    h1{
+        font-size: 2.8rem;
+    }
+    span{
+        color:#E1306C;
+        //font-weight: bold;
+        font-size: 4.5rem;
+    }
+
+    div{
+        border: solid;
+        width: 50%;
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+}
 
 </style>
