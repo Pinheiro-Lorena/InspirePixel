6bfad277347d223009cc6ba05bf77bad865b1ec1
Pinheiro-Lorena <lorenapro554@gmail.com>
2025-12-05 10:37:38 -0300

    alterações2

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<script setup>
+</script>
+
+<template>
+<footer> 
+    <p>© 2024 Inspire Pixel. Todos os direitos reservados.</p>
+<nav>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</nav>
+
+</footer>
+</template>
+
+
+<style scoped lang="scss">
+footer {
+    background-color: #222;
+    color: #fff;
+    text-align: center;
+    padding: 20px 0;
+    margin-top: 20px;
+    border-radius: 10px;
+}
+</style>
--- src/components/Inspire.vue
+++ src/components/Inspire.vue
@@ -1,34 +1,45 @@
 <script setup>
 import Card from './Card.vue';
 
+import axios from "axios";
 
+//importamos o ref do vue
+import { ref } from "vue";
+
+//criamos uma variavel reativa para armazenar as imagens
+const imagens = ref([]);
+
+//função para buscar as imagens da api
+async function carregarImagens () {
+   const res = await axios.get("https://picsum.photos/v2/list?page=3&limit=30")
+
+   imagens.value = res.data;
+   console.log(res)
+}
+
+carregarImagens();
 </script>
 
 <template>
-<h2>Inspire-se</h2>
+<h2>Inspire-se:</h2>
 <section class="inspire">
-<Card imagem="https://img.wattpad.com/a0e6267c31ae7890ba404683d461064c30390e3a/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f75367855396251364e62515330513d3d2d313437343438323334312e313766323064323834636466396466333138343338303333383839352e6a7067?s=fit&w=720&h=720"/>
-<Card imagem="https://pbs.twimg.com/media/EzqU02vVUAInbKn.jpg"/>
-<Card imagem="https://i.pinimg.com/736x/d7/a0/86/d7a0860b6eb4adb8697ac9423fedb99b.jpg"/>
-<Card imagem="https://i.pinimg.com/736x/bc/2b/f7/bc2bf7d2c1dc0bbb5f76d61c9b52d75c.jpg"/>
-<Card imagem="https://i.pinimg.com/564x/37/6e/28/376e28c91291e8b10cfe7a7d3f7205c1.jpg"/>
-<Card imagem="https://i.pinimg.com/1200x/f3/2b/0d/f32b0dc89dae79d4e213fdbcf3043fdf.jpg"/>
-<Card imagem="https://i.pinimg.com/564x/3d/22/77/3d2277b08a3f2b5328d8a52c8d94701b.jpg"/>
-<Card imagem="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6bEzrIqUV-r3AIgbsgXY6roh7gQXP_fI0og&s""/>
-<Card imagem="https://e1.pxfuel.com/desktop-wallpaper/780/841/desktop-wallpaper-iphone-pinterest-nature-aesthetic.jpg"/>
-<Card imagem="https://i.pinimg.com/236x/da/95/bb/da95bb35ce7184606986334e200ebf72.jpg"/>
-<Card imagem="https://i.pinimg.com/736x/6e/bf/07/6ebf074b268d924cb30a31cf5256b34c.jpg"/>
-<Card imagem="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ01liNreeQz2p0BsVAJgLjNpcLEszMuhHG36RtHdiw6LTuisT8LMamfFe08pl3aBgAgO0&usqp=CAU"/>
+<Card
+v-for="img in imagens"
+:imagem="img.download_url"
+/>
 
 
 </section>
 </template>
 
 <style scoped lang ="scss">
-.inspire, h2{
+.inspire {
     display: flex;
+    border-radius: 20px;
+    flex-wrap: wrap;
+    gap: 10px;
+    justify-content: center;
     
-    gap: 20px;
 
 }
 </style>
